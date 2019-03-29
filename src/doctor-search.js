271741782ce1566c575a6d3332d4ca89a058584f
samstoia <samstoia@gmail.com>
2019-03-29 09:25:59 -0700

    create class and methods to search doctor names and conditions

--- src/doctor-search.js
+++ src/doctor-search.js
@@ -1,1 +1,46 @@
 import $ from 'jquery';
+
+
+export default class DoctorSearch {
+  constructor(searchName, searchCondition) {
+    this.searchName = searchName;
+    this.searchCondition = searchCondition;
+    this.apiKey = process.env.exports.apiKey;
+    this.url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${searchName}&location=or-portland&user_key=${this.apiKey}`;
+    this.conditionUrl = `https://api.betterdoctor.com/2016-03-01/doctors?query=${searchCondition}&location=or-portland&user_key=${this.apiKey}`;
+  }
+
+  getName(searchName) {
+
+  return new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+
+      } else {
+        reject(Error(request.statusText));
+      }
+    }
+    request.open("GET", this.url, true);
+    request.send();
+  });
+
+  getCondition(searchName) {
+
+  return new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+
+      } else {
+        reject(Error(request.statusText));
+      }
+    }
+    request.open("GET", this.conditionUrl, true);
+    request.send();
+  });
+}
